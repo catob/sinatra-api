136977eb8d899c4d71cf0b16ccec56ae430aa511
cato b <cato.benjaminsen@gmail.com>
2019-12-06 19:25:02 +0100

    Extract book class and return

---
+++ book.rb
@@ -0,0 +1,14 @@
+class Book
+  include Mongoid::Document
+
+  field :title, type: String
+  field :author, type: String
+  field :isbn, type: String
+
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :isbn, presence: true
+
+  index({ title: 'text' })
+  index({ isbn:1 }, { unique: true, name: "isbn_index" })
+end
