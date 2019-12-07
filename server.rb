5de0c20edb1745045f5ef1259c7eb720886ffb3e
cato b <cato.benjaminsen@gmail.com>
2019-12-07 15:42:26 +0100

    Add post request route

--- server.rb
+++ server.rb
@@ -4,6 +4,7 @@ require 'mongoid'
 
 require_relative 'book'
 require_relative 'book_serializer'
+require_relative 'helper'
 
 # DB Setup
 Mongoid.load! 'mongoid.config'
@@ -32,6 +33,17 @@ namespace '/api/v1' do
     book = Book.find(id)
     halt(404, { message: 'Book not found' }.to_json ) unless book
     BookSerializer.new(book).to_json
+  end
+
+  post '/books' do
+    book = Book.new(json_params)
+    if book.save
+      response.headers['Location'] = "#{base_url}/api/v1/#{book.id}"
+      status 201
+    else
+      status 422
+      body BookSerializer.new(book).to_json
+    end
   end
 
   get '/times' do
